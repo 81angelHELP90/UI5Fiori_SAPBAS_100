72e3a9c1d0ec9ebb2f4d5dc42268f72bff653c1e
81angelHELP90 <vangel338@gmail.com>
2025-02-03 15:15:19 +0000

    primer commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.order.es5.ordertest1.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
